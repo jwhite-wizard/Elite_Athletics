cd607691fe00ec7911085e9239282577380cf065
Jacob White <jwhite4870@gmail.com>
2017-12-09 09:49:54 -0600

    Commenting out navbar for now until we have a redux store of shirts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+// import GoogleSearch from 'google-search';
 const API_KEY = 'AIzaSyAXP_vdckTWNja0brFi5XcugpyVJO_KPUE';
+
+// GoogleSearch({key: API_KEY, term: 'cool shirts'}, function(response)){
+//     console.log(response);
+// };
+
 // We want to create a new component that produces
 // some html
 const App = () => {
-    return <div>Gina's Bomb Shirts</div>;
+    return (
+        <div>
+            /*Eventually it would be nice to have a custom search bar, but we need to
+                have a database of shirts that we can search by item id or some sort of
+                tag system.
+             */
+        </div>
+        );
 }
 
 // Take this component's generated html and put it on the page
